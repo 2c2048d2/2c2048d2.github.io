0a574b95733cf845e26099b83220c9b300bb3df4
2c2048d2 <dyq233@qq.com>
2023-07-22 10:52:40 +0800

    rm dist

--- dist/assets/index-1b6635a0.js
+++
@@ -1,2 +0,0 @@
-import{_ as t,r as n,o as s,c as a,a as c,F as r,b as l}from"./index-cd27b43f.js";const d={data(){return{codeData:`def test()
-   print('hello world')`}}},_=l("h2",null,"blog",-1);function i(p,h,u,f,e,m){const o=n("highlightjs");return s(),a(r,null,[_,c(o,{code:e.codeData},null,8,["code"])],64)}const x=t(d,[["render",i]]);export{x as default};
